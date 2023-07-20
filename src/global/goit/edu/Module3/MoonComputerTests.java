b7dd4bf6b72fbfdb9124d0852dd977d5f4c5634c
Denis Bunak <xisi926@ukr.net>
2023-07-20 16:20:30 +0300

    Did 2 homeworks

--- src/global/goit/edu/Module3/MoonComputerTests.java
+++ src/global/goit/edu/Module3/MoonComputerTests.java
@@ -1,6 +1,34 @@
-package global.goit.edu.Module3;
+import java.util.*;
 
 public class MoonComputerTests {
+    public void testBasicInputOutput() {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+        scanner.close();
+    }
 
-    
+    public void testMath() {
+
+        Scanner scanner = new Scanner(System.in);
+        int firstInt = scanner.nextInt();
+        int secondInt = scanner.nextInt();
+
+        if (firstInt != 0 & secondInt != 0) {
+
+            int sumOfInts = firstInt + secondInt;
+            int differentOfInts = firstInt - secondInt;
+            int multiplicationOfInts = firstInt * secondInt;
+            int degreeOfInts = (int) (firstInt / secondInt);
+            System.out.println(sumOfInts + " " + differentOfInts + " " + multiplicationOfInts + " " + degreeOfInts);
+            scanner.close();
+        }
+
+    }
+
+
+    //Test output on 24, 4 standard input
+    public static void main(String[] args) {
+        new MoonComputerTests().testMath();
+    }
 }
