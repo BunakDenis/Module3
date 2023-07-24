127f6f13493e583d629b667b351f3f86bdd9addd
Denis Bunak <xisi926@ukr.net>
2023-07-24 11:06:48 +0300

    Stoped on the 21 Homework

--- src/global/goit/edu/Module3/SaveStarShip.java
+++ src/global/goit/edu/Module3/SaveStarShip.java
@@ -1,11 +1,72 @@
-package global.goit.edu.Module3;
+import java.util.*;
 
 public class SaveStarShip {
-
     public int calculateDistance(int distance) {
+        if (distance < 0) {
+            return distance * -1;
+        }
 
-        int moduleOfNumber = Math.abs(distance);
-        return moduleOfNumber;
+        return distance;
+    }
+
+    public String[] getPlanets(String galaxy) {
+        if (galaxy.equals("Miaru")) {
+            return new String[] {"Maux", "Reux", "Piax"};
+        } else if (galaxy.equals("Milkyway")) {
+            return new String[] {"Earth", "Mars", "Jupiter"};
+        } else if (galaxy.equals("DangerBanger")) {
+            return new String[] {"Fobius", "Demius"};
+        } else {
+            return new String[0];
+        }
+    }
+
+    public String choosePlanet(long distanceToEarth) {
+        if (distanceToEarth < 45677) {
+            return "Earth";
+        } else {
+            return "Pern";
+        }
+    }
+
+    public int calculateFuelPrice(String fuel, int count) {
+        int price = 50;
+
+        if (fuel.equals("STAR100")) {
+            price = 70;
+        } else if (fuel.equals("STAR500")) {
+            price = 120;
+        } else if (fuel.equals("STAR1000")) {
+            price = 200;
+        }
+
+        return count * price;
+    }
+
+    public int roundSpeed(int speed) {
+        int firstDigit = speed / 10;
+        int lastDigit = speed % 10;
+
+        if (lastDigit >= 5) {
+            firstDigit++;
+        }
+
+        return firstDigit * 10;
+    }
+
+    public int calculateNeededFuel(int distance) {
+
+        int needFuel = 1000;
+
+        if (distance > 20) {
+
+            return needFuel + ((distance - 20) * 5);
+
+        } else {
+
+            return needFuel;
+
+        }
 
     }
 
@@ -15,5 +76,20 @@ public class SaveStarShip {
 
         //Should be 10
         System.out.println(ship.calculateDistance(-10));
+
+        //Should be [Fobius, Demius]
+        System.out.println(Arrays.toString(ship.getPlanets("DangerBanger")));
+
+        //Should be Earth
+        System.out.println(ship.choosePlanet(10));
+
+        //Should be 700
+        System.out.println(ship.calculateFuelPrice("STAR100", 10));
+
+        //Should be 60
+        System.out.println(ship.roundSpeed(55));
+
+        //Should be 1005
+        System.out.println(ship.calculateNeededFuel(21));
     }
 }
